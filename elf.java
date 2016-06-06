de83025406dbdfad648476690f4e340b47e04e6a
Sinnlos <domin_mal@wp.pl>
2016-06-06 23:56:31 +0200

    Create elf.java

---
+++ elf.java
@@ -0,0 +1,17 @@
+package przebudzenie;
+
+public class elf extends postac {
+	
+	public elf(){
+		
+		description = "Elf ";
+		
+		}
+	
+	public String opis(){
+		
+		return "posiada dwie specjalne umiejętnosci: widzenie w ciemności oraz super zręczność";
+	}
+	
+	
+}
