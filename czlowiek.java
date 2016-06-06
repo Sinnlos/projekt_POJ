018d8aae14dcab47ce64063415b01ce2ec81f325
Sinnlos <domin_mal@wp.pl>
2016-06-06 23:56:04 +0200

    Create czlowiek.java

---
+++ czlowiek.java
@@ -0,0 +1,18 @@
+package przebudzenie;
+
+public class czlowiek extends postac {
+	
+	public czlowiek(){
+		
+	description = "Człowiek ";
+	
+	}
+	
+	public String opis(){
+		
+		return "nie posiada specjalnych umiejętnosci";
+	
+	}
+	
+
+}
