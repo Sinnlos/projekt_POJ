fc4378953fcd0d2c438d857ae9e251218761f7c2
Sinnlos <domin_mal@wp.pl>
2016-06-14 12:15:54 +0200

    Update kreatorPostaci.java

--- kreatorPostaci.java
+++ kreatorPostaci.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 package przebudzenie;
 
 public class kreatorPostaci {
@@ -13,6 +17,10 @@ public static void main(String args []){
 		postac2 = new zbrojmistrz(postac2);
 		postac2 = new wojownik(postac2);
 		System.out.println(postac2.getDescription() + postac2.opis());
+	
+	 PrintWriter zapis = new PrintWriter("postac.txt");
+	      zapis.println(postac2.getDescription() + postac2.opis());
+	      zapis.close();
 		
 	}
 	
