6de97aa6488646a69b7500471aebb87c1ed8aecc
Sinnlos <domin_mal@wp.pl>
2016-06-18 23:48:39 +0200

    Update mag.java

--- mag.java
+++ mag.java
@@ -1,26 +1,23 @@
 package przebudzenie;
 
-public class mag extends profesja {
-	postac postac;
-	
-	public mag(postac postac2) {
-		// TODO Auto-generated constructor stub
-	}
+public class Mag extends Profesja {
+    Postac postac;
+    
+    public Mag(Postac postac){
+    
+    this.postac = postac;
+    }
+    
+    public String getDescription() {
 
-	public void Mag(postac postac){
-	
-	this.postac = postac;
-}
-
-	public String getDescription(){
-		
-		return postac.getDescription() + ", Mag";
-		
-		}		
-	
-	public String opis(){
-		return " posiada dwie specjalne umiejętnosci: czarowanie oraz eliksiry" + postac.opis();
-		}
+        
+        return postac.getDescription() +  ", Mag ";
+        
+        }       
+    
+    public String opis(){
+        return " czarowanie, eliksiry"+ postac.opis();
+        }
 
 
 }
