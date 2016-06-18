1022199e1349abce765d5757036faba3f968990f
Sinnlos <domin_mal@wp.pl>
2016-06-18 23:50:22 +0200

    Update wojownik.java

--- wojownik.java
+++ wojownik.java
@@ -1,27 +1,23 @@
 package przebudzenie;
 
-public class wojownik extends profesja {
-	
-	postac postac;
-	
-public wojownik(postac postac2) {
-		// TODO Auto-generated constructor stub
-	}
+public class Wojownik extends Profesja {
+    
+    Postac postac;
+    
 
-public void Wojownik(postac postac){
-	
-	this.postac = postac;
+public Wojownik(Postac postac){
+    
+    this.postac = postac;
 }
 
-	public String getDescription(){
-		
-		return postac.getDescription() + ", Wojownik";
-		
-		}
-	
-	public String opis(){
-	return "posiada cztery specjalne umiejętnosci: walka wręcz, walka mieczem, walka toporem oraz walka włócznią" + postac.opis();
-	}
-		
+    public String getDescription(){
+        
+        return postac.getDescription() + ", Wojownik ";
+        
+        }
+    
+    public String opis(){
+    return " walka wręcz, walka mieczem, walka toporem, walka włócznią" + postac.opis();
+    }
+        
 }
-
