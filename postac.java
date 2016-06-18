41f17c13852bd8de9a8c11f29422f3aa8a5b5499
Sinnlos <domin_mal@wp.pl>
2016-06-18 23:49:30 +0200

    Update postac.java

--- postac.java
+++ postac.java
@@ -1,13 +1,13 @@
 package przebudzenie;
 
-public abstract class postac {
-	
-	String description = "Nieznany opis";
-	
-	public String getDescription(){
-		
-		return description;
-	}
-	
-	public abstract String opis();
+public abstract class Postac {
+    
+    String description = "Nieznany opis";
+    
+    public String getDescription(){
+        
+        return description;
+    }
+    
+    public abstract String opis();
 }
