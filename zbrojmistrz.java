2e208c1c3f752925acf47a93bb043a2fdbf48cf1
Sinnlos <domin_mal@wp.pl>
2016-06-18 23:51:06 +0200

    Update zbrojmistrz.java

--- zbrojmistrz.java
+++ zbrojmistrz.java
@@ -1,26 +1,22 @@
 package przebudzenie;
 
-public class zbrojmistrz extends profesja{
-	postac postac;
-	
-public zbrojmistrz(postac postac2) {
-		// TODO Auto-generated constructor stub
-	}
-
-public void Zbrojmistrz(postac postac){
-	
-	this.postac = postac;
+public class Zbrojmistrz extends Profesja{
+    Postac postac;
+    
+public Zbrojmistrz(Postac postac){
+    
+    this.postac = postac;
 }
 
-	public String getDescription(){
-		
-		return postac.getDescription() + ", Zbrojmistrz";
-		
-		}
-	
+    public String getDescription(){
+        
+        return postac.getDescription() + ", Zbrojmistrz ";
+        
+        }
+    
 
-	public String opis(){
-		return " posiada jedną specjalną umiejętnosć: naprawa ekwipunku" + postac.opis();
-		}
-	
+    public String opis(){
+        return " naprawa ekwipunku" + postac.opis();
+        }
+    
 }
