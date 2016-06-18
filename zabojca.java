10d1005f9c561530cea772f1cf6cfde6f54e217b
Sinnlos <domin_mal@wp.pl>
2016-06-18 23:50:44 +0200

    Update zabojca.java

--- zabojca.java
+++ zabojca.java
@@ -1,23 +1,23 @@
 package przebudzenie;
 
-public class zabojca extends profesja {
-	postac postac;
-	
-public void Zabojca(postac postac){
-	
-	this.postac = postac;
+public class Zabojca extends Profesja {
+    Postac postac;
+    
+public Zabojca(Postac postac){
+    
+    this.postac = postac;
 }
 
-	public String getDescription(){
-		
-		return postac.getDescription() + ", Zabójca";
-		
-		}
+    public String getDescription(){
+        
+        return postac.getDescription() + ", Zabójca ";
+        
+        }
 
-	
-	public String opis(){
-		return"posiada trzy specjalne umiejętnosci: skradanie, trucizny oraz walka wręcz" + postac.opis();
-		}
+    
+    public String opis(){
+        return " skradanie, trucizny, walka wręcz" + postac.opis();
+        }
 
 
 }
